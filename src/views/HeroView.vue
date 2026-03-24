9c4169e52390710004e37b96e04f27b74fab296a
Sergey Medvedev <s.medvedev-vip@yandex.ru>
2026-03-24 20:57:34 +0300

    2 урок дз выполнено

--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -46,7 +46,10 @@
         <div class="row">
           <div class="col-lg-10 offset-lg-1">
             <div class="best__wrapper">
-              <div class="best__item">
+              <product-card />
+              <product-card />
+              <product-card />
+              <!-- <div class="best__item">
                 <img src="@/assets/img/coffee-1.jpg" alt="coffee" />
                 <div class="best__item-title">Solimo Coffee Beans 2kg</div>
                 <div class="best__item-price">10.73$</div>
@@ -60,7 +63,7 @@
                 <img src="@/assets/img/coffee-3.jpg" alt="coffee" />
                 <div class="best__item-title">AROMISTICO Coffee 1kg</div>
                 <div class="best__item-price">6.99$</div>
-              </div>
+              </div> -->
             </div>
           </div>
         </div>
@@ -71,8 +74,9 @@
 
 <script>
 import NavBarComponent from "@/components/NavBarComponent.vue";
+import ProductCard from "@/components/ProductCard.vue";
 
 export default {
-  components: {NavBarComponent},
+  components: {NavBarComponent, ProductCard},
 };
 </script>
